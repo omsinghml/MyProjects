b1f24bc749ab2a387a18a11785517f8c8ec07ffd
Om Singh <86463981+omsinghml@users.noreply.github.com>
2023-06-19 21:38:38 +0530

    Delete projects directory

--- projects/coloured_text.py
+++
@@ -1,5 +0,0 @@
-import colorama
-from colorama import Fore, Back, Style
-colorama.init(autoreset=True)
-
-print(Fore.BLUE+Back.YELLOW+"My name is Om Singh"+Fore.YELLOW+Back.BLUE+" and I am machhine learning enthusisast")
--- projects/tables1.py
+++
@@ -1,9 +0,0 @@
-with open('table.txt', 'w') as file:
-	for j in range(2, 21):
-		i = [j*n for n in range(11)]
-		for t in i:
-			if t == 0:
-				t = str(f'\n{t}')
-			t = f'{t}\n'
-			file.write(t)
-
